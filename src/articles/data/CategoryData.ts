7f9da7b5b925f4a71a82a4018812530f5efa9432
roney ruiz <roneyruizrojas@gmail.com>
2025-02-10 19:59:35 -0600

    feat: add category data

---
+++ src/articles/data/CategoryData.ts
@@ -0,0 +1,8 @@
+import { Category } from "../types/Category";
+import LanguageIcon from "@assets/icons/language-icon.svg";
+
+export const LanguageCategory: Category = {
+  name: "Language",
+  icon: LanguageIcon,
+  description: "Artículos sobre idiomas, aprendizaje, curiosidades y consejos.",
+};
