b891bc9bed87733ec9d76abf9283bb6c2dbb84f8
roney ruiz <roneyruizrojas@gmail.com>
2025-02-10 19:59:24 -0600

    feat: add author data

---
+++ src/articles/data/AuthorData.ts
@@ -0,0 +1,15 @@
+import { Author } from "../types/Author";
+import RoneyProfile from "@assets/imgs/snorlax-profile.jpg";
+import WilhemProfile from "@assets/imgs/lucario-profile.jpg";
+
+export const RoneyAuthor: Author = {
+  name: "Roney Ruiz Rojas",
+  profile: RoneyProfile,
+  contact: "https://www.instagram.com/_rruiz204/",
+};
+
+export const WilhemAuthor: Author = {
+  name: "Wilhem Alvarado Montero",
+  profile: WilhemProfile,
+  contact: "https://www.instagram.com/wil_234_/",
+};
---
+++ src/articles/types/Author.ts
@@ -0,0 +1,5 @@
+export interface Author {
+  name: string;
+  profile: string;
+  contact: string;
+};
