f4fef1dc46259e3dc848ba8f354d8edd230f2d29
roney ruiz <roneyruizrojas@gmail.com>
2025-02-10 20:01:18 -0600

    refactor: remove logo from navigation bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { JSX } from "react";
 import CodingImg from "@assets/imgs/hand-coding.png";
 import { Navbar } from "@components/common/organisms/Navbar";
+import { AuthorList } from "@components/common/organisms/AuthorList";
+import { ArticleList } from "@components/common/organisms/ArticleList";
 
 export const Home = (): JSX.Element => {
   return (
@@ -19,6 +21,13 @@ export const Home = (): JSX.Element => {
 
         <div className="">
           <img src={CodingImg} className="w-[400px] h-[400px]" />
+        </div>
+      </div>
+
+      <div className="flex min-h-screen bg-ternary-200">
+        <div className="flex-1 flex flex-col md:flex-row justify-center gap-10 md:gap-14 py-10 md:py-20 px-8">
+          <ArticleList />
+          <AuthorList />
         </div>
       </div>
     </div>
