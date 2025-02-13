fec85ce74638b8f50f04a75188aa578e9938c015
roney ruiz <roneyruizrojas@gmail.com>
2025-02-12 21:21:51 -0600

    feat: add a redirection in article item

--- src/components/common/molecules/ArticleItem.tsx
+++ src/components/common/molecules/ArticleItem.tsx
@@ -1,17 +1,19 @@
 import { JSX } from "react";
+import { Link } from "../atoms/Link";
 import { CaterogyList } from "../organisms/CaterogyList";
 import { Article as Props } from "@articles/types/Article";
 
-export const ArticleItem = ({ name, description, categories }: Props): JSX.Element => {
+export const ArticleItem = ({ name, description, categories, route }: Props): JSX.Element => {
   return (
-    <div className="flex flex-col gap-4 px-6 md:px-8 pt-8">
+    <div className="flex flex-col gap-6 px-6 md:px-8 pt-8">
       <div>
         <p className="text-2xl font-inter-semi mb-2 underline">{name}</p>
         <p className="text-details-100 font-inter-light line-clamp-3">{description}</p>
       </div>
 
-      <div>
+      <div className="flex justify-between">
         <CaterogyList categories={categories} />
+        <Link text="Read more" route={route} />
       </div>
     </div>
   );
