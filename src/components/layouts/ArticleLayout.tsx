8f3419477ae1b5e5d9c335a7892ce68a71e8a3fb
roney ruiz <roneyruizrojas@gmail.com>
2025-02-12 21:21:23 -0600

    feat: build article layout

--- src/components/layouts/ArticleLayout.tsx
+++ src/components/layouts/ArticleLayout.tsx
@@ -1,12 +1,27 @@
 interface Props {
+  title: string;
+  cover: string;
+  posted: string;
   children: React.ReactNode;
 };
 
-export const ArticleLayout = ({ children }: Props) => {
+export const ArticleLayout = ({ children, title, posted, cover }: Props) => {
   return (
-    <div>
-      <p>Article Layout</p>
-      {children}
+    <div className="bg-ternary-200">
+      <div className="min-h-screen flex justify-center">
+        <div className="w-full md:w-[800px] flex flex-col bg-primary">
+          <div className="pt-16 px-10">
+            <p className="font-inter-bold text-6xl underline mb-5">{title}</p>
+            <p className="font-inter-semi text-details-100">{posted} / Content score: 87</p>
+
+            <div className="mt-8">
+              <img src={cover} className="w-full max-h-[500px] rounded-xl" />
+            </div>
+          </div>
+
+          {children}
+        </div>
+      </div>
     </div>
   );
 };
